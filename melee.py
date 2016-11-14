1f4e63e7832d3c05d2bc2589b399ef18d373778a
buhstech <buhstech@brawleyhigh.org>
2016-11-14 15:26:19 -0800

    Update melee.py

--- melee.py
+++ melee.py
@@ -10,3 +10,13 @@ pygame.init()
 size = (700, 500)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("World Meme Database")
+
+done = False
+clock = pygame.time.Clock()
+pygame.movie.Movie(falco.mpg)
+while not done:
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+          done = True
+      elif event.type == pypgame.KEYDOWN:
+        pygame.movie.Movie.play()
