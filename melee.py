959bf7cd54f8df46c37e2762f7858169ce4c39cf
buhstech <buhstech@brawleyhigh.org>
2016-11-14 10:32:26 -0800

    Create melee.py

---
+++ melee.py
@@ -0,0 +1,12 @@
+# Programmed by:
+# Mr. Gilder (corporate entity)
+# JJ (the resident president)
+# and Rydog (Hyaaaaah) Agundez
+# Smite is a bad game.
+
+import pygame
+pygame.init()
+
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("World Meme Database")
