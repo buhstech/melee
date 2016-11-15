e7c0b4d7e80efe1094532c91f3c2ecda80795c64
buhstech <buhstech@brawleyhigh.org>
2016-11-15 14:40:14 -0800

    Update melee.py

--- melee.py
+++ melee.py
@@ -4,14 +4,14 @@
 # and Rydog (Hyaaaaah) Agundez
 # Smite is a bad game.
 
-for i in range(1):
- meleechar = input("What character do you play in melee?")
- if meleechar == "falco":
-  print("Short hop laser is OP. Brandon has a bad falco.")
- elif meleechar == "mario":
-  print("ScorpionMaster")
- elif meleechar == "marth":
-  print("I didn't know you knew how to juggle. Ryan is 2gud.")
- elif meleechar == "roy":
-  for i in range(10):
-   print("Roy is our boy.")
+
+meleechar = input("What character do you play in melee?")
+if meleechar == "falco":
+ print("Short hop laser is OP. Brandon has a bad falco.")
+elif meleechar == "mario":
+ print("ScorpionMaster")
+elif meleechar == "marth":
+ print("I didn't know you knew how to juggle. Ryan is 2gud.")
+elif meleechar == "roy":
+ for i in range(10):
+  print("Roy is our boy.")
